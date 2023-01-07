b70cd798a8ba97662b9d1ae5e4396cb4d3e8b17e
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2023-01-07 21:07:05 +0100

    update base image add tests

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Just a little container to expose a file system with a basic auth
 
+This project uses:
+
+* [task](https://taskfile.dev/)
+* [venom](https://github.com/ovh/venom)
+
+
 # Run
 
 See tests/docker-compose.yml for example
@@ -9,7 +15,19 @@ See tests/docker-compose.yml for example
 # Build
 
 ```
-./build.sh
+task
+```
+
+# Tests
+
+```
+task tests
+```
+
+# Release
+
+```
+task release
 ```
 
 # .htpasswd
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-> /etc/nginx/.htpasswd
+/bin/true > /etc/nginx/.htpasswd
 
 echo "Generation of .htpassword"
 for i in ${LST_USERS_PASSWORDS}
 do
-    user=$(echo $i | cut -d: -f1)
-    password=$(echo $i | cut -d: -f2)
+    user=$(echo "$i" | cut -d: -f1)
+    password=$(echo "$i" | cut -d: -f2)
     #echo "$user:$password" >> /etc/nginx/.htpasswd
-    htpasswd -b -c /etc/nginx/.htpasswd.$$  $user $password
+    htpasswd -b -c /etc/nginx/.htpasswd.$$  "$user" "$password"
     cat /etc/nginx/.htpasswd.$$ >> /etc/nginx/.htpasswd
     rm /etc/nginx/.htpasswd.$$
 done
 
-exec $@
+exec "$@"
